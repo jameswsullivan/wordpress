d760ec633296da6803ea104c599e1053b22a6672
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-06-15 17:27:21 +0000

    Initial commit of php8.4

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+service apache2 start
+
+cd ${SITE_CONFIG_DIR}
+a2ensite *
+
+a2enmod proxy
+a2enmod proxy_http
+a2enmod rewrite
+a2enmod headers
+a2enmod ssl
+a2enmod http2
+a2enmod status
+a2enmod remoteip
+#a2dismod mpm_prefork
+#a2enmod mpm_event
+
+service apache2 restart
+service apache2 reload
+
+exec "$@"
+tail -f /dev/null
+
